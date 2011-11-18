df8cdb877ade3241b848462c6ed3680f0a53c32b
Steve Lianoglou <slianoglou@gmail.com>
2011-11-18 11:41:20 -0500

    Handy Command class
    Makes creating command line scripts easy, deals with junk required to process/check input/output streams.

--- Python/seqtools/cmdline.py
+++ Python/seqtools/cmdline.py
@@ -1,4 +1,4 @@
-import sys
+import os, sys, time
 from optparse import OptionParser
 
 from seqtools import io
@@ -9,6 +9,7 @@ class Command(object):
     If input (first argument) is missing or "-" reads stdin
     If output (seond argument) is misgin or "-" writes to sdout
     
+    We assume that the infile argument always comes immediately before output.
     Takes care of checking input files / streams etc.
     
     Command line options added automatically:
@@ -16,7 +17,14 @@ class Command(object):
         * -f/--force : overwrite output if already exists
     """
     
-    def __init__(self, parser):
+    def __init__(self, name, parser, in_arg=0, out_arg=1):
+        """Create Command Line program
+        
+        set in_arg,out_arg to None if you do not want to process an
+        input/output file
+        
+        """
+        self.name = name
         self.to_stdout = False
         self.from_stdin = False
         self.infile = None
@@ -24,12 +32,12 @@ class Command(object):
         self.overwrite = False
         self.options = None
         self.args = None
-        self.stats = {}
+        self.stats = []
         self.__process_parser(parser)
-        self.__init_file_streams()
+        self.__init_file_streams(in_arg, out_arg)
     
     def __process_parser(self, parser):
-        parser.add_optoin('-f', '--force', dest="force", default=False,
+        parser.add_option('-f', '--force', dest="force", default=False,
                           action="store_true", help="Ignore any precautions")
         parser.add_option("-v", "--verbose", dest="verbose", default=False,
                           action="store_true", help="Make some noise")
@@ -37,33 +45,31 @@ class Command(object):
         self.parser = parser
         
         
-    def __init_file_streams(self):
-        if len(self.args) == 0:
-            self.infile = sys.stdin
-            self.from_stdin = True
-            self.outfile = sys.stdout
-            self.to_stdout = True
-        if len(self.args) > 0:
-            if self.args[0] == '-':
+    def __init_file_streams(self, in_arg=0, out_arg=1):
+        if in_arg is not None:
+            if len(self.args) - 1 < in_arg or self.args[in_arg] == '-':
                 self.infile = sys.stdin
                 self.from_stdin = True
             else:
-                self.infile = self.args[0]
-                if not os.path.isfile(infile):
+                self.infile = self.args[in_arg]
+                if not os.path.isfile(self.infile):
                     parser.error("Cannot read input file: " + self.infile)
                 self.infile = io.xopen(self.infile, 'r')
                 self.from_stdin = False
-            self.outfile = sys.stdout
-            self.to_stdout = True
-        if len(self.args) > 1:
-            self.outfile = self.args[2]
-            if os.path.isfile(self.outfile) and not self.overwrite:
-                parser.error("Destination file already exists")
-            outfile = io.xopen(outfile, 'w')
-            close_out = True
-        
 
-    def error(self, msg, print_help=True):
+        if out_arg is not None:
+            if len(self.args) - 1 < out_arg or self.args[out_arg] == '-':
+                self.outfile = sys.stdout
+                self.to_stdout = True
+            else:
+                self.outfile = self.args[out_arg]
+                if os.path.isfile(self.outfile) and not self.options.force:
+                    self.error("Outfile already exists, " \
+                               "use -f/--force to overwite")
+                self.outfile = io.xopen(self.outfile, 'w')
+                self.to_stdout = False
+
+    def error(self, msg, print_help=False):
         """Produce an error and exit"""
         self.parser.error(msg)
     
@@ -72,7 +78,27 @@ class Command(object):
             sys.stderr.write(msg + "\n")
         else:
             sys.stdout.write(msg + "\n")
-    
-    
 
+    def add_stat(self, name, value):
+        self.stats.append((name, value))
+
+    def run(self, what):
+        t0 = time.time()
+        what(self)
+        elapsed = time.time() - t0
+        report = sys.stderr
+        
+        if not self.to_stdout:
+            report = sys.stdout
+            self.outfile.close()
+        if not self.from_stdin:
+            self.infile.close()
+        
+        report.write('\n')
+        report.write('========== ' + self.name + ' Finished ==========\n')
+        report.write('Elapsed time:  %.2f seconds\n' % elapsed)
+        for (name,val) in self.stats:
+            report.write('%s: %s\n' % (name, val))
+        report.write("\n")
+    
 # END : Class Command
