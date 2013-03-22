9991faec8b29a677bee154d6fcdd97b00d67099c
Steve Lianoglou <slianoglou@gmail.com>
2013-03-21 21:32:04 -0400

    adding alignerParams

--- R/pkg/R/alignerParams.R
+++ R/pkg/R/alignerParams.R
@@ -1,7 +1,8 @@
 ## Construct aligner-specific ScanBamParams for some common alignment options
 
-bwaSamseParams <- function(max.mismatch=NULL, with.alt.alignments=FALSE, ...) {
-  p <- ScanBamParam()
+bwaSamseParams <- function(max.mismatch=NULL, with.alt.alignments=FALSE,
+                           flag=scanBamFlag(isUnmappedQuery=FALSE), ...) {
+  p <- ScanBamParam(flag=flag)
   tags <- c("X0", "X1", "XM")
   if (with.alt.alignments) {
     tags <- c(tags, "XA")
